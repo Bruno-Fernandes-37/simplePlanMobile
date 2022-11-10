5502242b3d57b100aa837da16fb413d0d7019bd7
SebastienFrancois <sebastien.devlopweb@gmail.com>
2022-11-10 13:24:06 +0100

    feat[mobile]: updateUser requests

--- src/api/users.js
+++ src/api/users.js
@@ -16,4 +16,54 @@ export const USERS = {
       }
     }
   `,
+  getUser: gql`
+    query Query($getUserId: ID) {
+      getUser(id: $getUserId) {
+        _id
+        email
+        username
+        picture
+        preferred_language
+        role
+      }
+    }
+  `,
+  updateEmail: gql`
+  mutation Mutation($updateUserInfosAsUserId: ID!, $email: String) {
+    updateUserInfosAsUser(id: $updateUserInfosAsUserId, email: $email) {
+      username
+      email
+    }
+  }
+  `,
+  updateUsername: gql `
+  mutation Mutation($updateUserInfosAsUserId: ID!, $username: String) {
+    updateUserInfosAsUser(id: $updateUserInfosAsUserId, username: $username) {
+      username
+    }
+  }
+  `,
+  updatePicture: gql `
+  mutation Mutation($updateUserInfosAsUserId: ID!, $picture: String) {
+    updateUserInfosAsUser(id: $updateUserInfosAsUserId, picture: $picture) {
+      picture
+      username
+    }
+  }
+  `,
+  updatePreferredLanguage: gql`
+  mutation Mutation($updateUserInfosAsUserId: ID!, $preferredLanguage: String) {
+    updateUserInfosAsUser(id: $updateUserInfosAsUserId, preferred_language: $preferredLanguage) {
+      username
+      preferred_language
+    }
+  }
+  `,
+  updatePassword : gql`
+  mutation Mutation($updateUserInfosAsUserId: ID!, $password: String) {
+    updateUserInfosAsUser(id: $updateUserInfosAsUserId, password: $password) {
+      email
+    }
+  }
+  `,
 };
