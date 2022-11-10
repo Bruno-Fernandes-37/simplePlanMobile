9d2561052cd1f583bea42e6e7207ecedb42d9041
SebastienFrancois <sebastien.devlopweb@gmail.com>
2022-11-10 13:24:46 +0100

    feat[mobile]: styles

--- src/components/MainInfos/styles.js
+++ src/components/MainInfos/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 import { Colors } from "../../assets/styles/colors";
 
 const styles = StyleSheet.create({
-    container: {display: "flex", flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', paddingHorizontal: 8, marginBottom: 24},
+    container: {display: "flex", flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', paddingHorizontal: 10, marginBottom: 24},
     username: {color: "black", fontSize: 24, fontWeight: 'bold', textTransform: 'capitalize', marginBottom: 4},
     role: {color: Colors.veryPeri, fontSize: 16, fontWeight: '600', textTransform: 'capitalize', marginBottom: 4, textShadowColor: Colors.veryPeri, textShadowRadius: 2 }
 
---
+++ src/components/TextInput/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    input: {
+        marginTop: 5,
+        height: 40,
+        width: 250,
+        borderRadius: 5,
+        padding: 10,
+        backgroundColor: 'white'
+      },
+});
+
+export default styles;
